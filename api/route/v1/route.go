a89eb77829c8d4d23f501f13e4067d0da6fa2903
CS@ Spread BY <spreadby@businessmatching.co.uk>
2023-09-18 09:02:38 +0700

    Add sample

--- api/route/v1/route.go
+++ api/route/v1/route.go
@@ -11,4 +11,5 @@ func Setup(env *bootstrap.Env, timeout time.Duration, routerV1 *gin.RouterGroup)
 	publicRouterV1 := routerV1.Group("")
 
 	HomeRouter(env, timeout, publicRouterV1)
+	SampleRouter(env, timeout, publicRouterV1)
 }
